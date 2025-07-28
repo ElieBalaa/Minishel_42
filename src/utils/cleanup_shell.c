5c2415277b47e4ba9241f79cf793e27bf4431de5
Elias Al Balaa <eal-bala@c1r5s3.42beirut.com>
2025-07-28 22:38:14 +0300

    4

--- src/utils/cleanup_shell.c
+++ src/utils/cleanup_shell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cleanup_shell.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oiskanda <oiskanda@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eal-bala <eal-bala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 21:45:42 by oiskanda          #+#    #+#             */
-/*   Updated: 2025/07/28 20:28:03 by oiskanda         ###   ########.fr       */
+/*   Updated: 2025/07/28 22:37:27 by eal-bala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	cleanup_shell(t_minishell *sh)
 {
 	int	i;
 
-	ft_putendl_fd("exit", 1);
 	gc_cleanup_all(sh);
 	if (sh->is_interactive)
 		rl_clear_history();
